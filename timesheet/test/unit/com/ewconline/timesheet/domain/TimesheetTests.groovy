2fb339f40ae972cd743b89e55c40482a2e9eacc2
eldrave <eldrave@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-08 02:52:59 +0000

    added domain class tests

---
+++ timesheet/test/unit/com/ewconline/timesheet/domain/TimesheetTests.groovy
@@ -0,0 +1,17 @@
+package com.ewconline.timesheet.domain
+
+import grails.test.*
+
+class TimesheetTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
