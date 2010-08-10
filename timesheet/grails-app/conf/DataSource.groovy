18a3192681cd190908287d60d3fe4978ef09dcc2
eldrave <eldrave@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-10 22:49:59 +0000

    updated to include configuration for BCT mysql server

--- timesheet/grails-app/conf/DataSource.groovy
+++ timesheet/grails-app/conf/DataSource.groovy
@@ -1,8 +1,9 @@
 dataSource {
     pooled = true
-    driverClassName = "org.hsqldb.jdbcDriver"
-    username = "sa"
-    password = ""
+    //driverClassName = "org.hsqldb.jdbcDriver"
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "timesheet"
+    password = "timesheet"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -12,9 +13,13 @@ hibernate {
 // environment specific settings
 environments {
     development {
-        dataSource {
+        """dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
+            url = "jdbc:hsqldb:mem:devDB" """
+           dataSource {
+               dbCreate="create-drop"
+               url="jdbc:mysql://dev.bct-llc.com:3306/timesheet"
+           
         }
     }
     test {
