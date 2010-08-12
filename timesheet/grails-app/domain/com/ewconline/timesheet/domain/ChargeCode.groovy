ff18da054bd19032b63bb1906183beb50913d32d
eldrave <eldrave@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-12 23:14:07 +0000

    added acegi files and created the security classes

--- timesheet/grails-app/domain/com/ewconline/timesheet/domain/ChargeCode.groovy
+++ timesheet/grails-app/domain/com/ewconline/timesheet/domain/ChargeCode.groovy
@@ -2,6 +2,11 @@ package com.ewconline.timesheet.domain
 
 class ChargeCode {
 
+    String chargeNumber
+    String descirption
+    
     static constraints = {
+        chargeNumber(blank:false, maxSize:100)
+        description(nullable:true, maxSize:1000)
     }
 }
