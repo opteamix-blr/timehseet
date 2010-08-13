874afd47354cfe5de6be09e4250886ebd0d3784b
eldrave <eldrave@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-13 21:18:03 +0000

    added to test creating timesheet entries

---
+++ timesheet/test/integration/com/ewconline/timesheet/TimesheetIntegrationTests.groovy
@@ -0,0 +1,37 @@
+package com.ewconline.timesheet
+
+import grails.test.*
+
+class TimesheetIntegrationTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testCreateTimeSheetEntry(){
+        Timesheet t = createTimesheet().save()
+        ChargeCode c = createChargeCode()
+        LaborCategory lc = createLaborCategory()
+        TimesheetEntry te1 = new TimesheetEntry(laborCategory:lc, chargeCode:c, dateWorked:new Date("07/01/2010"), hoursWorked:8.5)
+        assertEquals(new Date("07/01/2010"), te1.dateWorked)
+        assertNotNull(te1)
+        t.addToEntries(te1)
+
+        assertEquals(1, t.entries.count())
+    }
+
+
+    private LaborCategory createLaborCategory(){
+        return new LaborCategory(name:"Sys Eng 3", description:"System Engineer 3")
+    }
+
+    private ChargeCode createChargeCode(){
+        return new ChargeCode(chargeNumber:"EPM II - BA5 - TTO 3C", description:"Test charge #")
+    }
+     private Timesheet createTimesheet(){
+        return new Timesheet(startDate:new Date("07/01/2010"), endDate:new Date("07/15/2010"))
+    }
+}
