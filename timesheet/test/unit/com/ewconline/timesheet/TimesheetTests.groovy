aecf58869df30036d36f29c55bd1c55da185de6b
eldrave <eldrave@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-13 21:18:28 +0000

    updated for testing timesheet entries

--- timesheet/test/unit/com/ewconline/timesheet/TimesheetTests.groovy
+++ timesheet/test/unit/com/ewconline/timesheet/TimesheetTests.groovy
@@ -17,25 +17,6 @@ class TimesheetTests extends GrailsUnitTestCase {
         assertEquals(t.endDate, new Date("07/15/2010"))
     }
 
-    void testCreateTimeSheetEntry(){
-        Timesheet t = createTimesheet()
-        ChargeCode c = createChargeCode()
-        LaborCategory lc = createLaborCategory()
-        TimesheetEntry te1 = new TimesheetEntry(laborCategory:lc, chargeCode:c, dateWorked:new Date("07/01/2010"), hoursWorked:8.5)
-        assertNotNull(te1)
-        t.addToEntries(te1)
-        
-        assertEquals(t.entries.count, 1)
-    }
-
-    private LaborCategory createLaborCategory(){
-        return new LaborCategory(name:"Sys Eng 3", description:"System Engineer 3")
-    }
-
-    private ChargeCode createChargeCode(){
-        return new ChargeCode(chargeNumber:"EPM II - BA5 - TTO 3C", description:"Test charge #")
-    }
-
     private Timesheet createTimesheet(){
         return new Timesheet(startDate:new Date("07/01/2010"), endDate:new Date("07/15/2010"))
     }
