275ff0d1b76330892107407de15c80b8c18e7442
eldrave <eldrave@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-13 20:33:57 +0000

    added some unit tests

---
+++ timesheet/test/unit/com/ewconline/timesheet/TimesheetEntryTests.groovy
@@ -0,0 +1,17 @@
+package com.ewconline.timesheet
+
+import grails.test.*
+
+class TimesheetEntryTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
