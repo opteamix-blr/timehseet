5abebecec6350a08026618796c653a653361fe04
carl.dea <carl.dea@gmail.com>
2010-08-13 04:36:57 +0000

    created relationships.

--- timesheet/grails-app/domain/com/ewconline/timesheet/domain/TimesheetEntry.groovy
+++ timesheet/grails-app/domain/com/ewconline/timesheet/domain/TimesheetEntry.groovy
@@ -1,7 +1,16 @@
 package com.ewconline.timesheet.domain
 
+/** This represents a row on a time sheet.
+*/
 class TimesheetEntry {
+    Task task
+    LaborCategory laborCategory
+    ChargeCode chargeCode
 
     static constraints = {
     }
+    
+    static hasMany = [workDays:WorkDay]
+
+    
 }
