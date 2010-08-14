858c046cb100848abed976da0ba57588438638b1
carl.dea <carl.dea@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-14 23:47:10 +0000

    corrected the package name on the domain classes

--- timesheet/grails-app/conf/SecurityConfig.groovy
+++ timesheet/grails-app/conf/SecurityConfig.groovy
@@ -4,7 +4,7 @@ security {
 
 	active = true
 
-	loginUserDomainClass = "com.ewconline.timesheet.domain.User"
-	authorityDomainClass = "com.ewconline.timesheet.domain.Role"
-	requestMapClass = "com.ewconline.timesheet.domain.RequestMap"
+	loginUserDomainClass = "com.ewconline.timesheet.User"
+	authorityDomainClass = "com.ewconline.timesheet.Role"
+	requestMapClass = "com.ewconline.timesheet.RequestMap"
 }
