d04c1a7a89ec6b776543de91f3a0730965865485
eldrave <eldrave@gmail.com>
2010-08-17 20:25:56 +0000

    initial revision

---
+++ timesheet/grails-app/domain/com/ewconline/timesheet/Workday.groovy
@@ -0,0 +1,12 @@
+package com.ewconline.timesheet
+
+class Workday {
+	
+	Date dateWorked
+	double hoursWorked
+	
+	static belongsTo = [timesheetEntry : TimesheetEntry]
+	
+    static constraints = {
+    }
+}
