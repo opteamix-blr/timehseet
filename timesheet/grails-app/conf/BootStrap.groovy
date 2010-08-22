ef544df9a83f643faf0d3bbd952fafc84929194e
carl.dea <carl.dea@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-22 07:16:53 +0000

    setup logging for controllers in config and add admin user for development and test environment for bootstrap.groovy.

--- timesheet/grails-app/conf/BootStrap.groovy
+++ timesheet/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,43 @@
+import com.ewconline.timesheet.Role;
+import com.ewconline.timesheet.User;
+
+import grails.util.Environment;
+
 class BootStrap {
 
     def init = { servletContext ->
-    }
+		
+    	switch(Environment.current) {
+			case Environment.DEVELOPMENT:
+			case Environment.TEST:
+				createAdminUserIfRequired()
+				break;
+			case Environment.PRODUCTION:
+				break;
+		}
+		
+	}
     def destroy = {
     }
+
+	void createAdminUserIfRequired() {
+		if (!User.findByUsername("admin")) {
+			println "Creating an admin user for " + Environment.current + " environment"
+			log.debug "Creating an admin user for $Environment.current environment"
+			def adminRole = new Role(description:"Admin Role",
+				authority:"all"
+			).save()
+			
+			def user = new User(username:"admin",
+				passwd:"p@ssw0rd1",
+				//userRealName:"Administrator",
+				description:"Dev Admin user"
+			)
+			adminRole.addToPeople(user)
+			log.debug "Created successfully username:" + user?.username
+		} else {
+			log.debug "Existing admin user, skipping creation"
+		}
+	}
+	
 }
