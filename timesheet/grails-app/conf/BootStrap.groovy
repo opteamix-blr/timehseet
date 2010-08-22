ada22867fbbf520de9a8f6bb34d1ce173cccf660
carl.dea <carl.dea@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-22 07:50:44 +0000

    fixed some debugging statements

--- timesheet/grails-app/conf/BootStrap.groovy
+++ timesheet/grails-app/conf/BootStrap.groovy
@@ -22,21 +22,20 @@ class BootStrap {
 
 	void createAdminUserIfRequired() {
 		if (!User.findByUsername("admin")) {
-			println "Creating an admin user for " + Environment.current + " environment"
-			log.debug "Creating an admin user for $Environment.current environment"
+			println "Creating an admin user for $Environment.current environment"
 			def adminRole = new Role(description:"Admin Role",
 				authority:"all"
 			).save()
 			
 			def user = new User(username:"admin",
 				passwd:"p@ssw0rd1",
-				//userRealName:"Administrator",
+				userRealName:"Administrator",
 				description:"Dev Admin user"
 			)
 			adminRole.addToPeople(user)
-			log.debug "Created successfully username:" + user?.username
+			println "Created successfully username:" + user?.username
 		} else {
-			log.debug "Existing admin user, skipping creation"
+			println "Existing admin user, skipping creation"
 		}
 	}
 	
