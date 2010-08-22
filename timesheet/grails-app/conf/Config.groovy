9a6864da1ac7cf9572102385134f8791adb4c51e
carl.dea <carl.dea@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-22 07:50:44 +0000

    fixed some debugging statements

--- timesheet/grails-app/conf/Config.groovy
+++ timesheet/grails-app/conf/Config.groovy
@@ -66,7 +66,9 @@ log4j = {
     //appenders {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
-	debug  'grails.app.controller'
+	info   'grails.app.conf'
+	debug  'grails.app.controller',
+	       'grails.app.conf'
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
