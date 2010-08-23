e9504e1620aef9d9f2d96c3948d40eec80f3de9b
carl.dea <carl.dea@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-23 01:37:08 +0000

    added test users for the test and dev environment for integration tests.

--- timesheet/grails-app/conf/BootStrap.groovy
+++ timesheet/grails-app/conf/BootStrap.groovy
@@ -10,15 +10,27 @@ class BootStrap {
     	switch(Environment.current) {
 			case Environment.DEVELOPMENT:
 			case Environment.TEST:
+				createRolesIfRequired()
 				createAdminUserIfRequired()
+				createTestUsers()
 				break;
 			case Environment.PRODUCTION:
 				break;
 		}
-		
+
 	}
     def destroy = {
     }
+
+	void createRolesIfRequired() {
+		def employeeRole = new Role(description:"Employee Role",
+			authority:"self"
+		).save()
+		
+		def approverRole = new Role(description:"Approver Role",
+			authority:"approve"
+		).save()
+	}
 
 	void createAdminUserIfRequired() {
 		if (!User.findByUsername("admin")) {
@@ -26,17 +38,34 @@ class BootStrap {
 			def adminRole = new Role(description:"Admin Role",
 				authority:"all"
 			).save()
-			
+
 			def user = new User(username:"admin",
 				passwd:"p@ssw0rd1",
 				userRealName:"Administrator",
 				description:"Dev Admin user"
 			)
 			adminRole.addToPeople(user)
+
 			println "Created successfully username:" + user?.username
 		} else {
 			println "Existing admin user, skipping creation"
 		}
+	}
+	void createTestUsers() {
+		def user1 = new User(username:"emp1",
+				passwd:"p@ssw0rd1",
+				userRealName:"John Doe",
+				description:"Employee"
+		)
+		Role.findByAuthority("self").addToPeople(user1)
+		def user2 = new User(username:"approver1",
+			passwd:"p@ssw0rd1",
+			userRealName:"Jane Doe",
+			description:"Employee and Approver"
+		)
+		
+		Role.findByAuthority("self").addToPeople(user2)
+		Role.findByAuthority("approve").addToPeople(user2)
 	}
 	
 }
