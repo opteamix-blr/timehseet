3d70f6f8aa721be2955bc8a847a4e883c622f2f3
carl.dea <carl.dea@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-08-30 02:51:39 +0000

    Empty units to test controllers. Grails generates the controllers so they are assumed to work.

---
+++ timesheet/test/unit/com/ewconline/timesheet/ChargeCodeControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.ewconline.timesheet
+
+import grails.test.*
+
+class ChargeCodeControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
