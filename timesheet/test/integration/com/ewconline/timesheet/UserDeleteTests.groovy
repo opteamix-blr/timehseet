eac84b692dd7be5cc8b2de38456c083d12f19bb6
eldrave <eldrave@gmail.com>
2010-09-06 18:31:39 +0000

    initial revision

---
+++ timesheet/test/integration/com/ewconline/timesheet/UserDeleteTests.groovy
@@ -0,0 +1,17 @@
+package com.ewconline.timesheet
+
+import grails.test.*
+
+class UserDeleteTests extends ParentTimesheetTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
