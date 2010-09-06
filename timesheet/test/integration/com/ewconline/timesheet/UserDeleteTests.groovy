361829832651cfdb2d7ba50a58eae701d1d08d34
eldrave <eldrave@gmail.com>
2010-09-06 18:43:36 +0000

    Completed UserDeleteTests.groovy

--- timesheet/test/integration/com/ewconline/timesheet/UserDeleteTests.groovy
+++ timesheet/test/integration/com/ewconline/timesheet/UserDeleteTests.groovy
@@ -11,7 +11,25 @@ class UserDeleteTests extends ParentTimesheetTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testCreateAndDeleteUser() {
+		def User u = super.createUser ("tuser", "Test User")
+		def User tu = User.get(u.id)
+		assertNotNull tu
+		
+		tu.delete()
+		
+		tu = User.get(u.id)
+		assertNull tu
     }
+	
+	void testCreateAndDeleteUserTimesheet(){
+		def User u = super.createUser ("tuser", "Test User")
+		def Timesheet ts = super.createTimesheet (u, new Date("07/01/2010"), new Date("07/15/2010"))
+		assertNotNull ts
+		
+		u.delete()
+		
+		def Timesheet foundt = Timesheet.get(ts.id)
+		assertNull foundt		
+	}
 }
