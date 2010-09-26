8c4f02bc349a271bcd2628534909274d4bc29434
carl.dea <carl.dea@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-09-26 17:05:57 +0000

    initial revision.

---
+++ timesheet/test/unit/com/ewconline/timesheet/TimesheetManagerServiceTests.groovy
@@ -0,0 +1,18 @@
+package com.ewconline.timesheet
+
+import grails.test.*
+
+class TimesheetManagerServiceTests extends GrailsUnitTestCase {
+    
+	protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
