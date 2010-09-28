85d2f00fcf2bf48244e4ffcc53d4c4f1d1744205
eldrave <eldrave@gmail.com>
2010-09-28 22:55:36 +0000

    forgot to add the tag library

---
+++ timesheet/grails-app/taglib/timesheet/TotalsTagLib.groovy
@@ -0,0 +1,14 @@
+package timesheet
+import com.ewconline.timesheet.*;
+
+class TotalsTagLib {
+	static namespace="gt"
+	
+	def timesheetEntryTotal = { attrs ->
+		def totalHours = 0 
+		for(d in attrs.days){
+			totalHours += d.hoursWorked
+		}
+		out << totalHours
+	}
+}
---
+++ timesheet/test/unit/timesheet/TotalsTagLibTests.groovy
@@ -0,0 +1,17 @@
+package timesheet
+
+import grails.test.*
+
+class TotalsTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
