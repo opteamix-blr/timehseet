1634470d01a8a6ea7fcd668f15779e9438a9d71d
carl.dea <carl.dea@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-10-12 07:12:03 +0000

    open, approve, disaprove actions for Approve Workflow.

---
+++ timesheet/test/unit/com/ewconline/ApproverControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.ewconline
+
+import grails.test.*
+
+class ApproverControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
