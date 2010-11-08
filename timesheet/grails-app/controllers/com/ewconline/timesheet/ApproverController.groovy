1c450a3f9e46808961e46286a86616432151b5b6
carl.dea <carl.dea@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2010-11-08 05:59:58 +0000

    approvers can approve or disapprove a timesheet.

--- timesheet/grails-app/controllers/com/ewconline/timesheet/ApproverController.groovy
+++ timesheet/grails-app/controllers/com/ewconline/timesheet/ApproverController.groovy
@@ -7,40 +7,13 @@ class ApproverController {
 	def approverListTimesheet = {
         params.max = Math.min(params.max ? params.int('max') : 10, 100)
 		
-		def timesheetList = [Timesheet.findByApproveState("pending")]
+		def timesheetList = [Timesheet.findByCurrentState(timesheetManagerService.SIGNED)]
         [timesheetList: timesheetList, timesheetInstanceTotal: timesheetList.size()]
     }
 	
 	def userApprove = {
 	
-		def user = User.get(session.user.id)
-		Timesheet timesheetInstance = Timesheet.get(params.id)
 		
-		
-		if (params.version) {
-			def version = params.version.toLong()
-			if (timesheetInstance.version > version) {
-				timesheetInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'timesheet.label', default: 'Timesheet')] as Object[], "Another user has updated this Timesheet while you were editing")
-				render(view: "edit", model: [timesheetInstance: timesheetInstance])
-				return
-			}
-		}
-		
-		// if open allow state to be pending
-		if (timesheetInstance.approveState == "open") {
-			timesheetInstance.approveState = "pending"
-		} else {
-			flash.message = "${message(code: 'default.approveState.message', args: [message(code: 'timesheet.label', default: 'Timesheet'), timesheetInstance.id])}"
-			redirect(controller:"timesheet", action: "listTimesheets")
-			return
-		}
-		
-		if (!timesheetInstance.hasErrors() && timesheetInstance.save(flush: true)) {
-			flash.message = "${message(code: 'default.updated.message', args: [message(code: 'timesheet.label', default: 'Timesheet'), timesheetInstance.id])}"
-			redirect(controller:"timesheet", action: "show", id: timesheetInstance.id)
-		} else {
-			render(controller:"timesheet", view: "edit", model: [timesheetInstance: timesheetInstance])
-		}
     }
 	
 	
@@ -60,16 +33,9 @@ class ApproverController {
 				}
 			}
 			
-			// if open allow state to be pending
-			if (timesheetInstance.approveState == "pending") {
-				timesheetInstance.approveState = "approved"
-			} else {
-				flash.message = "${message(code: 'default.approveState.message', args: [message(code: 'timesheet.label', default: 'Timesheet'), timesheetInstance.id])}"
-				redirect(controller:"approver", action: "approverListTimesheet")
-				return
-			}
 			
-			if (!timesheetInstance.hasErrors() && timesheetInstance.save(flush: true)) {
+			if (!timesheetInstance.hasErrors()) {
+				timesheetManagerService.approve(timesheetInstance)
 				flash.message = "${message(code: 'default.updated.message', args: [message(code: 'timesheet.label', default: 'Timesheet'), timesheetInstance.id])}"
 				redirect(action: "approverListTimesheet")
 			}
@@ -92,16 +58,12 @@ class ApproverController {
 			}
 			// TODO MUST LOG COMMENT AS TO WHY DISAPPROVE
 			// if open allow state to be pending
-			if (timesheetInstance.approveState == "pending") {
-				timesheetInstance.approveState = "open"
-			} else {
-				flash.message = "${message(code: 'default.approveState.message', args: [message(code: 'timesheet.label', default: 'Timesheet'), timesheetInstance.id])}"
-				redirect(controller:"approver", action: "approverListTimesheet")
-				return
-			}
+
 			
-			if (!timesheetInstance.hasErrors() && timesheetInstance.save(flush: true)) {
+			if (!timesheetInstance.hasErrors()) {
+				
 				flash.message = "${message(code: 'default.updated.message', args: [message(code: 'timesheet.label', default: 'Timesheet'), timesheetInstance.id])}"
+				timesheetManagerService.disapprove(timesheetInstance)
 				redirect(action: "approverListTimesheet")
 			}
 	}
