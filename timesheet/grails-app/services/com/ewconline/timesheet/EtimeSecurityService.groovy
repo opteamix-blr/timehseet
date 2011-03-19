42b2dc905173496842693737784ef9f2fe99afe4
carl.dea@gmail.com <carl.dea@gmail.com@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2011-03-19 16:38:57 +0000

    fixed bug relating to roles for a logged in user.

--- timesheet/grails-app/services/com/ewconline/timesheet/EtimeSecurityService.groovy
+++ timesheet/grails-app/services/com/ewconline/timesheet/EtimeSecurityService.groovy
@@ -6,13 +6,16 @@ class EtimeSecurityService {
     static String APPROVER_ROLE = 'approver_role'
     static String ADMIN_ROLE = 'administrator_role'
 
-    def boolean isUserInRole(String username, String[] rolesText) {
+    def boolean isUserInRole(username,  rolesText) {
         def user = User.findByUsername(username)
         if (user) {
-            Role role = null
+            def userRoles = []
+            for (role in user.authorities) {
+                userRoles.add(role.authority)
+            }
+
             for (rt in rolesText) {
-                role = Role.findByAuthority(rt)
-                if (rt == role.authority) {
+                if (userRoles.contains(rt)) {
                     return true
                 }
             }
