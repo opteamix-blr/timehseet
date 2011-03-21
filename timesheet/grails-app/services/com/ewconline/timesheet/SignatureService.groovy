55eeb95779b11d847cb48cc15069a86d4d29116c
eldrave@gmail.com <eldrave@gmail.com@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2011-03-21 19:44:50 +0000

    updated User.groovy to use different table for mysql support.  updated SignatureService.groovy to use GUID

--- timesheet/grails-app/services/com/ewconline/timesheet/SignatureService.groovy
+++ timesheet/grails-app/services/com/ewconline/timesheet/SignatureService.groovy
@@ -3,12 +3,16 @@ package com.ewconline.timesheet
 class SignatureService {
 
     static transactional = true
-	static Hash hasher = new RabinHashFunction()
-	
+    static Hash hasher = new RabinHashFunction()
+
     def signTimesheet(ts, user) {
-		ts.signature = hasher.hash(user.email)
-		log.debug "The hash for user " + user.email + " is " + String.valueOf(ts.signature)
+        if(user.guid){
+            ts.signature = hasher.hash(user.guid)
+        }else{
+            ts.signature = hasher.hash(user.email)
+        }
+        log.debug "The hash for user " + user.email + " is " + String.valueOf(ts.signature)
     }
-	
-	
+
+
 }
