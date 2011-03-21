f7162cec36fb32b6bfc4e571d26a7a33fc8db93f
carl.dea@gmail.com <carl.dea@gmail.com@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2011-03-21 07:23:49 +0000

    Framework stubbed a unit test.

---
+++ timesheet/test/unit/com/ewconline/timesheet/TaskAssignmentApprovalTests.groovy
@@ -0,0 +1,17 @@
+package com.ewconline.timesheet
+
+import grails.test.*
+
+class TaskAssignmentApprovalTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
