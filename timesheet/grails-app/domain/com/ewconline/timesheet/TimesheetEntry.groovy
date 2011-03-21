f542f97ede446d3f0692f142aabef7e7d0ade89b
carl.dea <carl.dea@gmail.com>
2011-03-21 07:20:08 +0000

    added a current state of the timesheet entry like Timesheet object. Updated if a user is an approver for a particular entry.

--- timesheet/grails-app/domain/com/ewconline/timesheet/TimesheetEntry.groovy
+++ timesheet/grails-app/domain/com/ewconline/timesheet/TimesheetEntry.groovy
@@ -1,32 +1,44 @@
 package com.ewconline.timesheet
 
 /** This represents a row on a time sheet.
-*/
+ */
 class TimesheetEntry extends AuditableObject implements Comparable{
-	static belongsTo = [timesheet:Timesheet]
-	List workdays
-	TaskAssignment taskAssignment
+    static belongsTo = [timesheet:Timesheet]
+    List workdays
+    TaskAssignment taskAssignment
+
+    /* State - transition - State
+     * NOT_STARTED - saving - OPEN_SAVED
+     * OPEN_SAVED - signing - SIGNED
+     * OPEN_SAVED - saving - OPEN_SAVED
+     * SIGNED - approving - APPROVED
+     * SIGNED - disapproving - OPEN_NOT_SAVED
+     * APPROVED - disapproving - OPEN_NOT_SAVED
+     * OPEN_NOT_SAVED - saving - OPEN_SAVED
+     */
+    String currentState
 
     String toString(){
         return taskAssignment.toString()
     }
 
     static constraints = {
-		taskAssignment()
+        taskAssignment()
+        currentState(blank:false)
     }
     static hasMany = [workdays : Workday,
-					  notes: Note]
+        notes: Note]
 	
-	int compareTo(obj) {
-		taskAssignment?.task?.name.compareTo(obj.taskAssignment?.task?.name)
-	}
+    int compareTo(obj) {
+        taskAssignment?.task?.name.compareTo(obj.taskAssignment?.task?.name)
+    }
     def sumHours() {
-		float total=0.0
-		workdays.each { wd ->
-			if (wd.hoursWorked){
-				total = total +  wd.hoursWorked
-			}
-		}
-		total.round(2)
-	}
+        float total=0.0
+        workdays.each { wd ->
+            if (wd.hoursWorked){
+                total = total +  wd.hoursWorked
+            }
+        }
+        total.round(2)
+    }
 }
