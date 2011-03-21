4f8128de6d325bd6cdfcba87e8406e840d530640
carl.dea@gmail.com <carl.dea@gmail.com@c01cceb3-f366-6c7e-f52a-200990b70fa0>
2011-03-21 07:22:10 +0000

    allows approvers to only approve task assignments assigned to them.

--- timesheet/grails-app/controllers/com/ewconline/timesheet/ApproverController.groovy
+++ timesheet/grails-app/controllers/com/ewconline/timesheet/ApproverController.groovy
@@ -1,97 +1,97 @@
 package com.ewconline.timesheet
 
 class ApproverController {
-	def timesheetManagerService
+    def timesheetManagerService
     def index = { }
 	
-	def approverListTimesheet = {
+    def approverListTimesheet = {
         params.max = Math.min(params.max ? params.int('max') : 10, 100)
-		def timesheetList = Timesheet.createCriteria().list {
-			and {
-				eq('currentState', 'SIGNED')
-			}
-			maxResults(params.max)
-			order('lastUpdated', 'desc')
-		}
+        def timesheetList = Timesheet.createCriteria().list {
+            and {
+                eq('currentState', 'SIGNED')
+
+            }
+            maxResults(params.max)
+            order('lastUpdated', 'desc')
+        }
 		
         [timesheetList: timesheetList, timesheetInstanceTotal: timesheetList.count()]
     }
 	
-	def userApprove = {
+    def userApprove = {
 	
 		
     }
 	
 	
-	def accountantApprove = {
+    def accountantApprove = {
 		
-		def user = User.get(session.user.id)
+        def user = User.get(session.user.id)
 		
-		Timesheet timesheetInstance = Timesheet.get(params.id)
+        Timesheet timesheetInstance = Timesheet.get(params.id)
+		
+        if (params.version) {
+            def version = params.version.toLong()
+            if (timesheetInstance.version > version) {
+                timesheetInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'timesheet.label', default: 'Timesheet')] as Object[], "Another user has updated this Timesheet while you were editing")
+                render(view: "edit", model: [timesheetInstance: timesheetInstance])
+                return
+            }
+        }
 		
 		
-		if (params.version) {
-			def version = params.version.toLong()
-			if (timesheetInstance.version > version) {
-				timesheetInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'timesheet.label', default: 'Timesheet')] as Object[], "Another user has updated this Timesheet while you were editing")
-				render(view: "edit", model: [timesheetInstance: timesheetInstance])
-				return
-			}
-		}
-		
-		
-		if (!timesheetInstance.hasErrors()) {
-			try {
-				timesheetManagerService.approve(timesheetInstance)
-			} catch (Exception e) {
-				flash.message = e.getMessage()
-			}
-		}
-		redirect(action: "approverListTimesheet")
-	}
+        if (!timesheetInstance.hasErrors()) {
+            try {
+                timesheetManagerService.approve(timesheetInstance, user, session.accountantRole, session.approverRole )
+            } catch (Exception e) {
+                flash.message = e.getMessage()
+            }
+        }
+        redirect(action: "approverListTimesheet")
+    }
 
-	def accountantDisapprove = {
+    def accountantDisapprove = {
 		
-			def user = User.get(session.user.id)
+        def user = User.get(session.user.id)
 			
-			Timesheet timesheetInstance = Timesheet.get(params.id)
+        Timesheet timesheetInstance = Timesheet.get(params.id)
 			
 			
-			if (params.version) {
-				def version = params.version.toLong()
-				if (timesheetInstance.version > version) {
-					timesheetInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'timesheet.label', default: 'Timesheet')] as Object[], "Another user has updated this Timesheet while you were editing")
-					render(view: "edit", model: [timesheetInstance: timesheetInstance])
-					return
-				}
-			}
-			// TODO MUST LOG COMMENT AS TO WHY DISAPPROVE
-			// if open allow state to be pending
+        if (params.version) {
+            def version = params.version.toLong()
+            if (timesheetInstance.version > version) {
+                timesheetInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'timesheet.label', default: 'Timesheet')] as Object[], "Another user has updated this Timesheet while you were editing")
+                render(view: "edit", model: [timesheetInstance: timesheetInstance])
+                return
+            }
+        }
+        // TODO MUST LOG COMMENT AS TO WHY DISAPPROVE
+        // if open allow state to be pending
 
 			
-			if (!timesheetInstance.hasErrors()) {
+        if (!timesheetInstance.hasErrors()) {
 				
-				flash.message = "${message(code: 'default.updated.message', args: [message(code: 'timesheet.label', default: 'Timesheet'), timesheetInstance.id])}"
-				timesheetManagerService.disapprove(timesheetInstance)
-				redirect(action: "approverListTimesheet")
-			}
-	}
+            flash.message = "${message(code: 'default.updated.message', args: [message(code: 'timesheet.label', default: 'Timesheet'), timesheetInstance.id])}"
+            timesheetManagerService.disapprove(timesheetInstance)
+            redirect(action: "approverListTimesheet")
+        }
+    }
 	
-	def approvedTimesheets = {
-		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+    def approvedTimesheets = {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
 		
-		def timesheets = Timesheet.createCriteria().list {
-			and {
-				eq('currentState', 'APPROVED')	
-			}
-			maxResults(params.max)
-			order('lastUpdated', 'desc')
-		}
-		[timesheetList: timesheets, timesheetInstanceTotal: timesheets.count()]
-	}
+        def timesheets = Timesheet.createCriteria().list {
+            and {
+                eq('currentState', 'APPROVED')
+            }
+            maxResults(params.max)
+            order('lastUpdated', 'desc')
+        }
+        [timesheetList: timesheets, timesheetInstanceTotal: timesheets.count()]
+    }
 
-	def viewTimesheet = {
-		def ts = Timesheet.get(params.id)
-		[timesheetInstance:ts]
-	}
+    def viewTimesheet = {
+        def ts = Timesheet.get(params.id)
+        [timesheetInstance:ts]
+    }
 }
