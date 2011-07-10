30f8e4b0f1cecc103e566cd82a695f141d5646db
eldrave <eldrave@gmail.com>
2011-07-10 14:31:14 +0000

    updated user creation ui to inlcude 1-M task assignments.  controller code not completed.

--- timesheet/web-app/js/taskAssignment.js
+++ timesheet/web-app/js/taskAssignment.js
@@ -1,20 +1,54 @@
-document.observe('dom:loaded', function() {
-    $("task.id").observe("change", respondToSelect);
+var taSourceRow;
+var taCounter;
 
+Event.observe(window, 'load', function() {
+    taCounter=1
+    taSourceRow1 = $('taSourceRow');
+    try{
+        taSourceRow = taSourceRow1.cloneNode(true);
+        $('taSourceRow').remove();
+        var newRow = taSourceRow.cloneNode(true);
+        newRow.id = "taDataRow" + taCounter.toString()
+        $('taTable').appendChild(newRow);
+    }catch (e){
+        alert(e);
+    }
 });
 
+function addNewRow(){
+    newRow = taSourceRow.cloneNode(true);
+    taCounter++;
+    newRow.id = "taDataRow" + taCounter.toString();
+    $('taTable').appendChild(newRow);
+}
 
 function respondToSelect(event)
 {
     try{
-       new Ajax.Updater("laborCategorySelect",
-          appName + "/lookup/laborCategory",
-          {method:'get', parameters: {selectedValue : $F("task.id")} }
-         );
-       new Ajax.Updater("chargeCodeSelect",
-          appName + "/lookup/chargeCode",
-          {method:'get', parameters: {selectedValue : $F("task.id")} }
-         );
-    } catch (e){alert(e)}
+        new Ajax.Updater("laborCategorySelect",
+            appName + "/lookup/laborCategory",
+            {
+                method:'get',
+                parameters: {
+                    selectedValue : $F("task.id")
+                    }
+                }
+        );
+    new Ajax.Updater("chargeCodeSelect",
+        appName + "/lookup/chargeCode",
+        {
+            method:'get',
+            parameters: {
+                selectedValue : $F("task.id")
+                }
+            }
+    );
+} catch (e){
+    alert(e)
+    }
+}
+
+function addRow(){
+    addNewRow()
 }
 
