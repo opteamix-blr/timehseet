96fcbc851a0f5c4450aa1900e2050a49985d807a
carl.dea <carl.dea@gmail.com>
2012-01-20 05:39:09 +0000

    Updated the state diagram. No more Open not saved state.

--- timesheet/grails-app/domain/com/ewconline/timesheet/Timesheet.groovy
+++ timesheet/grails-app/domain/com/ewconline/timesheet/Timesheet.groovy
@@ -20,9 +20,8 @@ class Timesheet extends AuditableObject{
      * OPEN_SAVED - signing - SIGNED
      * OPEN_SAVED - saving - OPEN_SAVED
      * SIGNED - approving - APPROVED
-     * SIGNED - disapproving - OPEN_NOT_SAVED
-     * APPROVED - disapproving - OPEN_NOT_SAVED
-     * OPEN_NOT_SAVED - saving - OPEN_SAVED
+     * SIGNED - disapproving - OPEN_SAVED
+     * APPROVED - disapproving - OPEN_SAVED
      */
     String currentState
     Long signature
